2d4553dd9fd66db057603019a7aef668c8bece3f
shridevi-244 <shrivel2007_bcs28@mepcoeng.ac.in>
2026-04-10 12:23:17 +0530

    Create ordereddict1.py

---
+++ PYTHON/SOLVED/COLLECTIONS/ordereddict1.py
@@ -0,0 +1,15 @@
+dict_values = OrderedDict()
+string = "abcdcd"
+
+for char in string:
+   if char in dict_values:
+       dict_values[char] += 1
+   else:
+       dict_values[char] = 1
+
+for key, value in dict_values.items():
+    if value == 1:
+       print ("First non repeated character is %s" % key)
+       break
+    else:
+       pass
